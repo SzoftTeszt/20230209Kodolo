2e59b4adf8b9ddfe8190ac09ceab806f84836ee6
Attila Jager <jagerattila@gmail.com>
2023-02-09 20:08:55 +0100

    Create Test

--- src/titkosito/Titkosito.java
+++ src/titkosito/Titkosito.java
@@ -14,8 +14,14 @@ public class Titkosito {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Decodolo k= new Decodolo();
-        System.out.println(k.dekodol("cccc"));
+        Decodolo k= new Decodolo();      
+        Codolo c = new Codolo();
+        
+        String kodolando= "Ez egy titkos jelszó!";
+        String titok= c.codolo(kodolando);
+        System.out.println("Titok:"+titok); 
+        System.out.println("Dekódolva"+k.dekodol(titok));
+                
     }
     
 }
